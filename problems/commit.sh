3765f7b80d6c68d7b8d6014c264b8de1e4db6f2f
Priyanshu <priyanshu0171@gmail.com>
2022-07-27 13:59:33 +0530

    new changes made to the reference

--- problems/commit.sh
+++ problems/commit.sh
@@ -7,4 +7,4 @@ git commit -m "new changes made to the reference"
 git push -f origin master
 GREEN='\033[0;32m'
 NC='\033[0m' 
-printf "\n***************************\n${GREEN}Git Hub Commit Successful!!${NC}\n***************************\n" 
+printf "${NC}\n***************************\n${GREEN}Git Hub Commit Successful!!${NC}\n***************************\n" 
