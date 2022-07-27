c8f2e804c8788f4ea8389b27f9bdd1c6618a1377
Priyanshu <priyanshu0171@gmail.com>
2022-07-27 13:49:21 +0530

    new changes made to the reference

--- problems/commit.sh
+++ problems/commit.sh
@@ -3,5 +3,5 @@ rm -f {*.exe,.*.exe}
 git add .
 git commit -m "new changes made to the reference"
 git push -f origin master
-GREEN = '\033[0;32m'
+GREEN = '\033[0;32m]'
 printf "${GREEN}Success!!" 
