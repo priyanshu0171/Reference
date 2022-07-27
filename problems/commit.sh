36b9916d0c491d20bcb6b11ec53a4097b176f4ec
Priyanshu <priyanshu0171@gmail.com>
2022-07-27 14:06:11 +0530

    new changes made to the reference

--- problems/commit.sh
+++ problems/commit.sh
@@ -5,9 +5,9 @@ GREEN='\033[0;32m'
 NC='\033[0m' 
 printf "${RED}Deleting .EXE Files...\n\n${YELLOW}"
 rm -f {*.exe,.*.exe}
-printf "${YELLOW} Adding Changes to .git${NC}\n"
+printf "${YELLOW}Adding Changes to .git${NC}\n\n"
 git add .
-printf "${YELLOW}Commiting changes to GitHub in ${RED}<MASTER>${NC} Branch\n\n${NC}"
+printf "${YELLOW}Commiting changes to GitHub in ${RED}<MASTER>${YELLOW} Branch\n\n${NC}"
 git commit -m "new changes made to the reference"
 git push -f origin master
 printf "${NC}\n***************************\n${GREEN}Git Hub Commit Successful!!${NC}\n***************************\n" 
